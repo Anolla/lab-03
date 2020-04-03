df7d33fac05283f3a0524daf57a24120fdd01a0b
Anolla <annehaddad96@yahoo.com>
2020-04-03 15:24:11 +0300

    lab3

--- js/app.js
+++ js/app.js
@@ -20,22 +20,36 @@ $(document).ready(function() {
             data.forEach(animalItem => {
                 let animal = new Image(animalItem);
                 animal.render();
-                animal.list();
                 renderSelection();
             });
+            list();
         });
     };
     readJson(1);
+
+
     let uiniqueKey = [];
-    Image.prototype.list = function() {
-        if (!uiniqueKey.includes(this.keyword)) {
-            uiniqueKey.push(this.keyword)
-            let $newOption = $('<option></option>');
-            $('select').append($newOption);
-            $($newOption).text(this.keyword);
-            $($newOption).attr('value', this.keyword);
-        }
+
+
+    function list() {
+        // console.log(uiniqueKey);
+        uiniqueKey = [];
+        $('select').empty();
+        $('select').append($('<option>Filter by Keyword</option>'));
+        // this function can be a prototype as well. (instead of val, pass this)
+        Image.all.forEach(val => {
+                if (!uiniqueKey.includes(val.keyword)) {
+                    uiniqueKey.push(val.keyword)
+                    let $newOption = $('<option></option>');
+                    $('select').append($newOption);
+                    $($newOption).text(val.keyword);
+                    $($newOption).attr('value', val.keyword);
+
+                }
+            })
+            // console.log(uiniqueKey);
     }
+
     const renderSelection = () =>
         $('select').on('change', () => {
             let selectValue = $('select').val();
@@ -44,13 +58,59 @@ $(document).ready(function() {
         })
     renderSelection();
 
+
     function pageRender() {
         $('#button1').on('click', function() {
+            Image.all = [];
             readJson(1);
+            // console.log(option1);
+
         })
         $('#button2').on('click', function() {
+            Image.all = [];
             readJson(2);
+            // console.log(option2);
+
         })
     }
     pageRender();
+
+    function sortBy(array, property) {
+        array.sort((a, b) => {
+            let firstItem = a[property];
+            let secondItem = b[property];
+            if (property === 'title') {
+                firstItem = firstItem.toUpperCase();
+                secondItem = secondItem.toUpperCase();
+            }
+
+            if (firstItem > secondItem) {
+                return 1;
+
+            } else if (firstItem < secondItem) {
+                return -1;
+            } else { return 0; }
+
+
+        })
+
+    }
+
+    $("#radio1").on('click', () => {
+        sortBy(Image.all, 'title');
+        $('#horns-rendered').html('');
+        Image.all.forEach(image => image.render());
+
+    })
+
+
+    $("#radio2").on('click', () => {
+        sortBy(Image.all, 'horns')
+        $('#horns-rendered').html('');
+        Image.all.forEach(image => image.render());
+    })
+
+    // renderSelection();
+
+
 });
